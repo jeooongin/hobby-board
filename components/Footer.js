4330b15d916e514ccb6def9199d646170b35446a
jeongin <kwjddls1919@naver.com>
2022-11-30 22:31:20 +0900

    Feat: Footer 컴포넌트 생성

---
+++ components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const FooterWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Footer = () => {
+  return (
+    <FooterWrapper>
+      <div>Copyright 2022. JeongIn All rights reserved.</div>
+    </FooterWrapper>
+  );
+};
+
+export default Footer;
