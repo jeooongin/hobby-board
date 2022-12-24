59dc220788bcb386b8920493ec61d2f4d0b7de41
jeongin <kwjddls1919@naver.com>
2022-12-24 21:26:52 +0900

    Design: 화면 분할

--- components/App.js
+++ components/App.js
@@ -1,12 +1,34 @@
 import React from "react";
+import styled from "styled-components";
 import Footer from "./Footer";
 import Header from "./Header";
+
+const Wrapper = styled.div`
+  margin-top: 10px;
+  display: flex;
+
+  & .first-section {
+    width: 30%;
+  }
+
+  & .second-section {
+    width: 40%;
+  }
+
+  & .third-section {
+    width: 30%;
+  }
+`;
 
 const App = ({ children }) => {
   return (
     <>
       <Header />
-      {children}
+      <Wrapper>
+        <div className="first-section"></div>
+        <div className="second-section">{children}</div>
+        <div className="third-section"></div>
+      </Wrapper>
       <Footer />
     </>
   );
