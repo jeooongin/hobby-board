593f571ce478d18f0379680e7867faee34539695
jeongin <kwjddls1919@naver.com>
2022-12-24 21:29:37 +0900

    Feat: Redux Toolkit 적용

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,29 @@
 import Head from "next/head";
 import React from "react";
+import { Provider } from "react-redux";
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 import App from "../components/App";
+import wrapper from "../store/configureStore";
 
 const GlobalStyles = createGlobalStyle`
  ${reset}
 `;
 
-const _app = ({ Component }) => {
+const _app = ({ Component, ...rest }) => {
+  const { store, props } = wrapper.useWrappedStore(rest);
+
   return (
     <>
-      <Head>
-        <title>hobby-board</title>
-      </Head>
-      <GlobalStyles />
-      <App>
-        <Component />
-      </App>
+      <Provider store={store}>
+        <Head>
+          <title>hobby-board</title>
+        </Head>
+        <GlobalStyles />
+        <App>
+          <Component {...props.pageProps} />
+        </App>
+      </Provider>
     </>
   );
 };
